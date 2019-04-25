2f4120fc89b13301b0897836e27d3b4f0b7367a6
Z1803002 <48362075+Z1803002@users.noreply.github.com>
2019-04-25 04:53:17 -0500

    Ainsley Updated 4:53 4/25

---
+++ Project/GymInfo/GymInfo/GymData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GymInfo
+{
+    public partial class GymData_Member : Form
+    {
+        public GymData_Member()
+        {
+            InitializeComponent();
+        }
+
+        private void GymData_Load(object sender, EventArgs e)
+        {
+            GymTables x = new GymTables();
+            x.Close();
+        }
+
+        private void mEMBERBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.mEMBERBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.gymDatabaseDataSet);
+
+        }
+
+        private void GymData_Load_1(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'gymDatabaseDataSet.MEMBER' table. You can move, or remove it, as needed.
+            this.mEMBERTableAdapter.Fill(this.gymDatabaseDataSet.MEMBER);
+
+        }
+    }
+}
---
+++ Project/GymInfo/GymInfo/GymTables.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GymInfo
+{
+    public partial class GymTables : Form
+    {
+        public GymTables()
+        {
+            InitializeComponent();
+        }
+
+        private void GymTables_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GymData_Member x = new GymData_Member();
+            x.Show();
+        }
+
+        private void membershipBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void classBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clubBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void employeeBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
